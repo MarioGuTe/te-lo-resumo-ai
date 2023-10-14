96212181f389e92862cebb972e05db8979d29667
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-14 19:13:29 -0300

    add imports and basic structure

--- src/molecules/Home.jsx
+++ src/molecules/Home.jsx
@@ -2,16 +2,21 @@ import Header from "../atoms/Header/Header";
 import Hero from "../atoms/Hero/Hero";
 import SubmitArticle from "../atoms/SubmitArticle/SubmitArticle";
 import SummarizedArticle from "../atoms/SummarizedArticle/SummarizedArticle";
+import s from "./particle/style.module.css";
 
 const Home = () => {
   return (
-    <div>
-      This is the homepage
-      <Header />
-      <Hero />
-      <SubmitArticle />
-      <SummarizedArticle />
-    </div>
+    <main>
+      <div className={s.gradient_container}>
+        <div className={s.gradient} />
+      </div>
+      <div className={s.app}>
+        <Header />
+        <Hero />
+        <SubmitArticle />
+        <SummarizedArticle />
+      </div>
+    </main>
   );
 };
 
