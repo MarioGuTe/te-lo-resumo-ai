31cb493ce41ecbcd8bfb2d3c570dde35bc7b7e92
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-14 19:09:31 -0300

    add basic structure and style

--- src/atoms/Header/Header.jsx
+++ src/atoms/Header/Header.jsx
@@ -1,5 +1,19 @@
+// import { logo } from "../../assets/";
+import spinner from "../../assets/purple-spinner.png";
+import s from "./particle/style.module.css";
+
 const Header = () => {
-  return <header>This is the header</header>;
+  return (
+    <header>
+      <div className={s.logo_container}>
+        <img src={spinner} alt="logo" />
+        <ul>
+          <li className={s.list_text}>Te Lo Resumo</li>
+        </ul>
+      </div>
+      <button>Github</button>
+    </header>
+  );
 };
 
 export default Header;
