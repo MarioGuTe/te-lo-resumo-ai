54b97b31bf61900ef939e3fd66ced1db2887e07e
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-15 03:16:18 -0300

    add content and style

--- src/atoms/Hero/Hero.jsx
+++ src/atoms/Hero/Hero.jsx
@@ -1,5 +1,18 @@
+import s from "./particle/style.module.css";
+
 const Hero = () => {
-  return <div>This is the Hero Section</div>;
+  return (
+    <section className={s.hero_section}>
+      <h1>
+        Resume artículos con <br />
+        <span>OpenAI GPT-4</span>
+      </h1>
+      <h2>
+        Agiliza tu lectura con SINTESIS <br />
+        un resumidor de artículos potenciado por inteligencia artificial
+      </h2>
+    </section>
+  );
 };
 
 export default Hero;
