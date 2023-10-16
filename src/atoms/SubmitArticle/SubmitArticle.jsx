e977b84e9d30a1e3d101f349832c82830f4206d7
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-16 02:24:32 -0300

    add basic logic

--- src/atoms/SubmitArticle/SubmitArticle.jsx
+++ src/atoms/SubmitArticle/SubmitArticle.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { linkIcon } from "../../assets";
 import s from "./particle/style.module.css";
 
 const SubmitArticle = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("submitted");
+  };
+
   return (
     <section className={s.input_section}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <img src={linkIcon} alt="link_icon" />
-        <input type="url" placeholder="ingresa una URL" value="" required />
+        <input
+          type="url"
+          placeholder="ingresa una URL"
+          onChange={(e) => setArticle({ ...article, url: e.target.value })}
+          value={article.url}
+          required
+        />
+        <button type="submit" className={s.submit_button}>
+          <p>↵</p>
+        </button>
       </form>
     </section>
   );
