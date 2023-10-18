14405d0bc3d4b519af0c152c1fd2131039defb30
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-18 17:49:06 -0300

    add structure and proptypes

--- src/atoms/SummarizedArticle/SummarizedArticle.jsx
+++ src/atoms/SummarizedArticle/SummarizedArticle.jsx
@@ -1,5 +1,39 @@
-const SummarizedArticle = () => {
-  return <div>This is Summarized Article</div>;
+import { loader } from "../../assets";
+import s from "./particle/style.module.css";
+
+import PropTypes from "prop-types";
+
+const SummarizedArticle = ({ isFetching, articleSummary, error }) => {
+  return (
+    <section className={s.summarized_section}>
+      {isFetching ? (
+        <img src={loader} alt="loader" />
+      ) : error ? (
+        <p>
+          Well, that wasn supposed to happen...
+          <br />
+          <span></span>
+        </p>
+      ) : (
+        articleSummary && (
+          <div>
+            <h2>
+              Article <span>Summary</span>
+            </h2>
+            <div>
+              <p>{articleSummary}</p>
+            </div>
+          </div>
+        )
+      )}
+    </section>
+  );
 };
 
 export default SummarizedArticle;
+
+SummarizedArticle.propTypes = {
+  articleSummary: PropTypes.string,
+  isFetching: PropTypes.bool,
+  error: PropTypes.any,
+};
