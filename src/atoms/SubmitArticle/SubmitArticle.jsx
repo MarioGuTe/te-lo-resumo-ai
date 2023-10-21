0a0b28ac00395cbb19b31efb30dfb46f2b49fb0e
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-21 04:31:22 -0300

    refactor logic to Home molecule

--- src/atoms/SubmitArticle/SubmitArticle.jsx
+++ src/atoms/SubmitArticle/SubmitArticle.jsx
@@ -1,52 +1,21 @@
-import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { linkIcon } from "../../assets";
-import { useLazyGetSummaryQuery } from "../../services/article";
-import UrlList from "../UrlList/UrlList";
-import SummarizedArticle from "../SummarizedArticle/SummarizedArticle";
 import s from "./particle/style.module.css";
 
-const SubmitArticle = () => {
-  const [article, setArticle] = useState({
-    url: "",
-    summary: "",
-  });
-
-  const [articleHistory, setArticleHistory] = useState([]);
-
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("articles")
-    );
-
-    if (articlesFromLocalStorage.length > 0) {
-      setArticleHistory(articlesFromLocalStorage);
-    }
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { data } = await getSummary({ articleUrl: article.url });
-
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedArticleHistory = [newArticle, ...articleHistory];
-      setArticle(newArticle);
-      setArticleHistory(updatedArticleHistory);
-
-      localStorage.setItem("articles", JSON.stringify(updatedArticleHistory));
-    }
-  };
-
+const SubmitArticle = ({ onFormSubmit, article, onFormChange }) => {
   return (
     <section className={s.input_section}>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onFormSubmit();
+        }}
+      >
         <img src={linkIcon} alt="link_icon" />
         <input
           type="url"
           placeholder="ingresa una URL"
-          onChange={(e) => setArticle({ ...article, url: e.target.value })}
+          onChange={(e) => onFormChange({ ...article, url: e.target.value })}
           value={article.url}
           required
         />
@@ -54,16 +23,14 @@ const SubmitArticle = () => {
           <p>↵</p>
         </button>
       </form>
-      <UrlList articleList={articleHistory} />
-      <div>
-        <SummarizedArticle
-          isFetching={isFetching}
-          error={error}
-          articleSummary={article.summary}
-        />
-      </div>
     </section>
   );
 };
 
 export default SubmitArticle;
+
+SubmitArticle.propTypes = {
+  onFormSubmit: PropTypes.func,
+  onFormChange: PropTypes.func,
+  article: PropTypes.any,
+};
